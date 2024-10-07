e895cd96f54a75a91f944b80ccad78decd2eacba
PROEO6EEXP <ekke.jakob.mesila@tktk.ee>
2024-10-07 23:07:07 +0300

    add week4 code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿string userPin;
+
+while (true)
+{
+    Console.WriteLine("Enter your PIN");
+    userPin = Console.ReadLine();
+    if(userPin == "1234")
+    {
+        Console.WriteLine("Welcome!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong PIN. Try again.");
+    }
+}
---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//for(int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"i equals {i + 1}");
+//}
+
+//for (int i = 10; i > 0; i--) //i-- = i - 1
+//{
+//    Console.WriteLine($"i equals {i}");
+//}
+
+
+int sum = 0;
+
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine($"i equals {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i;  //sum += i;
+}
+
+Console.WriteLine($"Final total {sum}");
